e1f20ad9cb22f9c3604d02a4e8d14f47da9d482d
Vojtech Vondra <vojtavondra@gmail.com>
2012-07-11 13:51:42 +0200

    Update assebmly info

--- NewsletterServer/Properties/AssemblyInfo.cs
+++ NewsletterServer/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NewsletterServer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NewsletterServer")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Vojtěch Vondra")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
