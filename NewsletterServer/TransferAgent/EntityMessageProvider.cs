4adad5ceb59024f0e45a64fdc96721724e1db5a8
Vojtech Vondra <vojtavondra@gmail.com>
2012-07-19 19:59:02 +0200

    Message providers for delivery

---
+++ NewsletterServer/TransferAgent/EntityMessageProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Objects;
+
+namespace NewsletterServer.TransferAgent
+{
+
+    /// <summary>
+    /// Message provider implementation using Entity Framework
+    /// </summary>
+    internal class EntityMessageProvider : MessageProvider
+    {
+
+        /// <summary>
+        /// Object context to access database
+        /// </summary>
+        NewsletterEntities context;
+
+
+        /// <summary>
+        /// Creates new database message provider
+        /// </summary>
+        /// <param name="objectContext">object context for entities</param>
+        internal EntityMessageProvider(NewsletterEntities objectContext)
+        {
+            context = objectContext;
+        }
+
+        /// <summary>
+        /// Loads message from the database
+        /// </summary>
+        /// <returns>list of messages waiting to be sent</returns>
+        internal override List<Message> GetUndeliveredMessages()
+        {
+            
+            var msgQuery = from m in context.Messages
+                            where m.status == Message.StatusWaiting
+                            select m;
+
+            var messages = new List<Message>();
+
+            foreach (var msg in msgQuery) {
+                messages.Add(new Message(msg, context));
+            }
+
+            return messages;
+        }
+
+    }
+}
---
+++ NewsletterServer/TransferAgent/MessageProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsletterServer.TransferAgent
+{
+
+    /// <summary>
+    /// General message provider
+    /// 
+    /// Possible sources: database, web service, etc.
+    /// </summary>
+    internal abstract class MessageProvider
+    {
+
+        /// <summary>
+        /// Gets a list of messages waiting to be sent
+        /// </summary>
+        /// <returns>list of messages to be sent</returns>
+        internal abstract List<Message> GetUndeliveredMessages();
+    }
+}
